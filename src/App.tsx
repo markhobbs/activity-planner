0fc7bce34a5cfacde2a08557a6ac4c0408756c22
Mark <hobbs1m@hotmail.com>
2024-12-11 17:17:15 +0100

    Removing layout nesting

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <React.StrictMode>
           <Header />
           <Progress />
           <Routes>
-            <Route path="/" element={<Activity ikey={1} />}>
-              <Route index path="/about" element={<About />} />
+              <Route path="/" element={<Activity ikey={1} />} />
+              <Route path="/about" element={<About />} />
               <Route path="/planner" element={<Planner />} />
               <Route path="/store" element={<Store />} />
               <Route path="/awards" element={<Awards />} />
               <Route path="*" element={<NoPage />} />
-            </Route>
           </Routes>
           <Footer />
-        </React.StrictMode>
       </BrowserRouter>
     );
   }
